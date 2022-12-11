d1fa948153ac264c167954ce72694b6f700b94f8
msalamey <m.salamey23@gmail.com>
2022-12-11 13:45:14 -0500

    updated auth file to work with graphQL

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,17 +6,20 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+   // removed res & next from parameters
+   authMiddleware: function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
+    // removed res.status... replaced with req
     if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
+      return req
+      // res.status(400).json({ message: 'You have no token!' });
     }
 
     // verify token and get user data out of it
@@ -28,12 +31,13 @@ module.exports = {
       return res.status(400).json({ message: 'invalid token!' });
     }
 
+    return req; 
+
     // send to next endpoint
-    next();
+    // next();
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
