dc8786fd874fec3724cd9c6aeab3e52e684845a4
msalamey <m.salamey23@gmail.com>
2022-12-11 16:23:01 -0500

    changes in file management and backend code to attempt to get functional

---
+++ client/build/precache-manifest.743b2494f548a5548df395d3db46cbb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce1797bf96961ce766c287722a23c3eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3917cb0ed7b3e8c275ba",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "3dfd32e6fcbb9ddfc803",
+    "url": "/static/js/2.27b3ac98.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27b3ac98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3917cb0ed7b3e8c275ba",
+    "url": "/static/js/main.5436e1e6.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
